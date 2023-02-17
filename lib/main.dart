4e25af5971014fe9958e87f83af311dbb7c8eec2
Mubashir Saeed <mubashir5085@gmail.com>
2023-02-18 00:56:35 +0500

    Splash screen Added

--- lib/main.dart
+++ lib/main.dart
@@ -1,24 +1,54 @@
+import 'dart:async';
+
+import 'package:al_shams/views/login_screen.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: MyHomePage(),
-    );
-  }
-}
-class MyHomePage extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(),
-      body: Center(),
+    return ScreenUtilInit(
+      designSize: Size(360, 390),
+      minTextAdapt: true,
+      splitScreenMode: true,
+      builder: (context, child) {
+        return GetMaterialApp(
+          home: child,
+        );
+      },
+      child: SplashScreen(),
+    );
+  }
+}
+
+class SplashScreen extends StatefulWidget {
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+  void initState() {
+    super.initState();
+    Timer.periodic(Duration(seconds: 3), (timer) {
+      Get.offAll(()=>LoginScreen());
+    });
+  }
+
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+      child: Image.asset('assets/images/logo_without_bg.png'),
     );
   }
 }
